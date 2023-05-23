df32db9a60b9acf6d7ed1a7637205a744e82bfdd
rafi1212122 <rafi1212122@outlook.com>
2023-05-23 21:20:51 +0700

    Add project files.

---
+++ Common/Program.cs
@@ -0,0 +1,25 @@
+﻿using Config.Net;
+
+namespace Common
+{
+    public static class Global
+    {
+        public static IConfig config = new ConfigurationBuilder<IConfig>().UseJsonFile("config.json").Build();
+    }
+
+    public interface IConfig
+    {
+        [Option(DefaultValue = VerboseLevel.Normal)]
+        VerboseLevel VerboseLevel { get; }
+
+        [Option(DefaultValue = "mongodb://localhost:27017/crepebh")]
+        string DatabaseUri { get; }
+        
+    }
+
+    public enum VerboseLevel
+    {
+        Normal = 0,
+        Debug = 1
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using PemukulPaku.Resources.Proto;
+
+namespace PemukulPaku
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello!");
+            GetPlayerTokenRsp getPlayerTokenRsp = new()
+            {
+                Msg = "Hello!"
+            };
+        }
+    }
+}
