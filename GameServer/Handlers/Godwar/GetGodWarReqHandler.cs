637b399651915241868c2be8e7533a3ca12b1877
rafi1212122 <rafi1212122@outlook.com>
2023-06-05 13:18:07 +0700

    handlers folder re-strucutring

---
+++ GameServer/Handlers/Godwar/GetGodWarReqHandler.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+using Common.Resources.Proto;
+
+namespace PemukulPaku.GameServer.Handlers
+{
+    [PacketCmdId(CmdId.GetGodWarReq)]
+    internal class GetGodWarReqHandler : IPacketHandler
+    {
+        public void Handle(Session session, Packet packet)
+        {
+            GetGodWarRsp Rsp = new() { retcode = GetGodWarRsp.Retcode.Succ };
+
+            GodWar godWar = new()
+            {
+                GodWarId = 1,
+                BeginTime = 0,
+                EndTime = (uint)Global.GetUnixInSeconds() * 12 / 10,
+                MaxSupportPoint = 95,
+                LobbyId = 1,
+                CurChapterId = 1,
+                RoleInfo = new()
+                {
+
+                }
+            };
+            godWar.ChapterLists.AddRange(new uint[] { 1, 2, 3 }.Select(x => new GodWarChapter() { ChapterId = x }));
+
+            session.Send(Packet.FromProto(Rsp, CmdId.GetGodWarRsp));
+        }
+    }
+}
